07631bf08865c5c4e6f216a6008166a0d2176cc6
David Anthoff <anthoff@berkeley.edu>
2024-07-14 17:57:04 +0200

    Add a diagnostic

--- src/testserver_main.jl
+++ src/testserver_main.jl
@@ -5,6 +5,12 @@ popfirst!(LOAD_PATH)
 import Sockets
 
 try
+    if isdir(ARGS[2])
+        c = read(joinpath(ARGS[2], "Manifest.toml"), String)
+        println("MANIFEST CONTENT")
+        println(c)
+    end
+
     TestItemServer.serve(ARGS[1], nothing, ARGS[2], ARGS[3], ARGS[4])
 
 catch err
